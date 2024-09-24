b0f4a5be0615f3a5434ace627fcccbd27794eee9
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-24 08:30:01 +0545

    initialize react app

--- server/schema.ts
+++ server/schema.ts
@@ -2,19 +2,19 @@ import { gql } from "graphql-tag";
 import { PROJECTS } from "./projects.js";
 import { USERS } from "./users.js";
 export const typeDefs = gql`
-  type Project {
-    id: ID!
-    userId: ID!
-    name: String!
-    description: String!
-  }
-
   type User {
     id: ID!
     name: String!
     username: String!
     email: String!
     phone: String!
+  }
+  type Project {
+    id: ID!
+    userId: ID!
+    name: String!
+    description: String!
+    user: User
   }
 
   type Query {
@@ -25,6 +25,12 @@ export const typeDefs = gql`
 `;
 
 export const resolvers = {
+  Project: {
+    user: (project: any) => {
+      return USERS.find((user) => user.id === project.userId);
+    },
+  },
+
   Query: {
     projects: () => PROJECTS,
     users: () => USERS,
