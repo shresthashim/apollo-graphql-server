9c23eb3f49b71111de84159ef5b42b87c3a6a176
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-24 08:12:12 +0545

    add user query

--- server/index.ts
+++ server/index.ts
@@ -4,22 +4,40 @@ import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
 import { gql } from "graphql-tag";
 import { PROJECTS } from "./projects.js";
+import { USERS } from "./users.js";
 
 const typeDefs = gql`
   type Project {
     id: ID!
+    userId: ID!
     name: String!
     description: String!
   }
 
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    email: String!
+    phone: String!
+  }
+
   type Query {
     projects: [Project]
+    users: [User]
+    getUser(id: ID!): User
   }
 `;
 
 const resolvers = {
   Query: {
     projects: () => PROJECTS,
+
+    users: () => USERS,
+
+    getUser: (parent: any, { id }) => {
+      return USERS.find((user) => user.id === id);
+    },
   },
 };
 
